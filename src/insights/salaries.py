3d8ce90766c8f8dcdce11086aa747ddc5e01f7a4
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-05 18:32:49 -0300

    feat: req 8

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,6 +1,8 @@
 from typing import Union, List, Dict
 from src.insights.jobs import ProcessJobs
 
+# from jobs import ProcessJobs
+
 
 class ProcessSalaries(ProcessJobs):
     def __init__(self):
@@ -34,9 +36,45 @@ class ProcessSalaries(ProcessJobs):
         )
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        if "min_salary" not in job or "max_salary" not in job:
+            raise ValueError("Job must have min_salary and max_salary")
+        try:
+            # Convert values to integers
+            min_salary = int(job["min_salary"])
+            max_salary = int(job["max_salary"])
+
+            # Check if values are >= 0
+            if min_salary < 0 or max_salary < 0:
+                raise ValueError(
+                    "Values of 'min_salary' and 'max_salary' must be greater than or equal to zero."
+                )
+
+            # Check if min_salary is less than or equal to max_salary
+            if min_salary > max_salary:
+                raise ValueError(
+                    "'min_salary' must be less than or equal to 'max_salary'."
+                )
+
+            # Check if salary is numeric
+            salary = int(salary)
+
+            # Check if salary is within the salary range
+            return min_salary <= salary <= max_salary
+        except (ValueError, TypeError):
+            raise ValueError(
+                "Values of 'min_salary', 'max_salary', and 'salary' must be numeric."
+            )
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
     ) -> List[Dict]:
         pass
+
+
+process_salaries = ProcessSalaries()
+# print(process_salaries.read("data/jobs.csv"))
+print(
+    process_salaries.matches_salary_range(
+        {"max_salary": 1000, "min_salary": 100}, 100
+    )
+)
