08b83849bb5d7d9dff75200bee4ff75b96703c29
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-05 14:08:18 -0300

    feat: req 6

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,7 +7,22 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        # max_salary = 0
+        # for job in self.jobs_list:
+        #     if job["max_salary"] != "" and job["max_salary"] != "invalid":
+        #         print(job["max_salary"])
+        #         print(type(job["max_salary"]))
+        #         if int(job["max_salary"]) > max_salary:
+        #             max_salary = int(job["max_salary"])
+        # return max_salary
+
+        return max(
+            [
+                int(job["max_salary"])
+                for job in self.jobs_list
+                if job["max_salary"] != "" and job["max_salary"] != "invalid"
+            ]
+        )
 
     def get_min_salary(self) -> int:
         pass
